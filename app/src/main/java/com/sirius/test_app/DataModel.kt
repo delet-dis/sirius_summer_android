bf79ab40caac09516d1b807a01a76695f27f8539
delet-dis <efimov.036@gmail.com>
2022-07-12 21:41:38 +0700

    🌅 added header and game image

--- app/src/main/java/com/sirius/test_app/DataModel.kt
+++ app/src/main/java/com/sirius/test_app/DataModel.kt
@@ -1,8 +1,8 @@
 package com.sirius.test_app
 
-data class DataModel(
-    val image: String = "https://ibb.co/37KzF03",
-    val logo: String = "https://ibb.co/GpbZg5t",
+class DataModel(
+    val image: String = "https://i.ibb.co/g3YVWD2/img-background.png",
+    val logo: String = "https://user-images.githubusercontent.com/47276603/178517025-133fcd0e-3173-4c0f-b71c-c4efe7b58e63.png",
     val name: String = "DoTA 2",
     val rating: Float = 4.9f,
     val gradeCnt: String = "70M",
--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -1,11 +1,91 @@
 package com.sirius.test_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
+import com.sirius.test_app.databinding.ActivityMainBinding
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val dataModel = DataModel()
+
+    private var statusBarHeight by Delegates.notNull<Int>()
+
+    private fun initBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+
+    private fun initSystemBarsDimensionChangesListener() =
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _: View?, insets: WindowInsetsCompat ->
+            statusBarHeight = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
+
+            changeTopBarHeight(statusBarHeight)
+
+            WindowInsetsCompat.CONSUMED
+        }
+
+    private fun changeTopBarHeight(statusBarHeight: Int) {
+        val buttonsTopInset = resources.getDimension(R.dimen.transparentCardTopInset).toInt()
+        val recalculatedButtonsTopInset = buttonsTopInset + statusBarHeight
+
+        with(binding) {
+            with(backCard.layoutParams as ViewGroup.MarginLayoutParams) {
+                topMargin = recalculatedButtonsTopInset
+            }
+
+            with(optionsCard.layoutParams as ViewGroup.MarginLayoutParams) {
+                topMargin = recalculatedButtonsTopInset
+            }
+        }
+    }
+
+    private fun loadHeaderImage() {
+        Glide.with(this)
+            .load(dataModel.image)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .apply(options)
+            .into(binding.headerImage)
+    }
+
+    private fun loadGameImage() {
+        Glide.with(this)
+            .load(dataModel.logo)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .apply(options)
+            .into(binding.gameImage)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initBinding()
+
+        initSystemBarsDimensionChangesListener()
+
+        loadHeaderImage()
+
+        loadGameImage()
+    }
+
+    private companion object {
+        private val options: RequestOptions = RequestOptions()
+            .centerCrop()
+            .placeholder(R.drawable.progress_animation)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .priority(Priority.HIGH)
+            .dontAnimate()
+            .dontTransform()
     }
 }
